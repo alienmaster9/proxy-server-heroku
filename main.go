ea32b8f7771a104d85e0a772156e1559eb28a4ef
Venkatesh Gajula <quickstreamingmedia@users.noreply.github.com>
2021-05-07 04:47:52 +0700

    added headers
    added headers

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 func main() {
     p, _ := proxy.New(proxy.ProxyOptions{
         Balancer: func(req *http.Request) (string, error) {
+            req.Header.Set("X-Forwarded-For", "14.1.32.0")
+            req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36")
             return os.Getenv("UPSTREAM_SERVER"), nil
         },
     })
